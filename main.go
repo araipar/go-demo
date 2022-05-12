57d55d5d8298e1a3427103ef04a2bf8d538bbb9b
Rai Paramaartha <adwitya.paramaartha@enseval.com>
2022-05-12 22:44:54 +0700

    making byte slice

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
+import "fmt"
+
 
 func main() {
 
 
-	cards := newDeck()
+	// cards := newDeck()
 	
-	hand, remainingDeck := deal(cards, 5) //  2 new variable in order to get multi value returnm function
+	// hand, remainingDeck := deal(cards, 5) //  2 new variable in order to get multi value returnm function
 
-	hand.print()
-	remainingDeck.print()
+	// hand.print()
+	// remainingDeck.print()
 
+	greeting := "Hi Araipur"
+	fmt.Println([]byte(greeting))
 
 	//cards.print()
 
