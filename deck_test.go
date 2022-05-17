7aeecdb2a3fe7c0cb9111d7396b3cfc9c1ba763a
Rai Paramaartha <araipars@gmail.com>
2022-05-18 00:23:44 +0700

    testing save to  deck and load deck

--- deck_test.go
+++ deck_test.go
@@ -1,5 +1,8 @@
 package main
-import "testing"
+import (
+	"testing"
+	"os"
+)
 
 // test file is named x_test.go
 
@@ -9,5 +12,29 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 	if len(d) != 52 {
 		t.Errorf("Expected deck Length of 52 but we got %d", len(d))
-	}                                               
+	}                 
+	
+	if d[0] != "Ace Of Spades"{
+		t.Errorf("Expected Ace Of Spades on top but we got %s", d[0])
+	}
+
+	if d[len(d) - 1] != "K Of Diamonds"{
+		t.Errorf("Expected K Of Diamonds on bottom but we got %s", d[len(d) - 1])
+	}
+	
+}
+
+func TestSaveToDeckAndTestNewDeckFromFile(t *testing.T){
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile ("_decktesting")
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck Length of 52 but we got %d", len(loadedDeck))
+	}  
+
+	os.Remove("_decktesting")
+
 }
