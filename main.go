d6983952eae1027a78be833d7bb324ae63ee514f
Rai Paramaartha <araipars@gmail.com>
2022-05-19 00:10:52 +0700

    defining map

--- main.go
+++ main.go
@@ -2,51 +2,16 @@ package main
 
 import "fmt"
 
-type contactInfo struct {
-	email string 
-	zipCode int
-}
-
-type person struct{
-	firstName string
-	lastName string
-	contactInfo
-}
-
-
 
 func main() {
 
-jim := person{
-	firstName: "Jim",
-	lastName: "Correy",
-	contactInfo: contactInfo {
-		email:"jim@me.com",
-		zipCode:17115,
-	},
+	colors := map[string]string{
+		"red" : "#ff0000",
+		"green" : "#1234556",
+	}
+
+
+	fmt.Println(colors)
+
 }
 // go is a pass by value language
-
-jim.updateName("Rain")
-
-jim.print()
-
-}
-func (p person) print(){
-	fmt.Printf("%+v",p)
-}
-
-func (pointerToPerson *person) updateName(newFirstName string){ 
-	(*pointerToPerson).firstName = newFirstName // * operator = give me the value this memory address is pointing at
-}
-
-//  0001       person{firstname:"Jim"...}
-// address^           value^
-
-// turn address into value with *address
-// turn value into address with   &value
-
-
-// use pointers to change Value Types like : int,float , string ,bool,structs
-// dont worry about pointers with Reference Types like : Slices, maps, channels , pointers , functions
-
