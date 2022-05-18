b09092eea1fca67841a6c32e9929fa19b8af3879
Rai Paramaartha <araipars@gmail.com>
2022-05-19 00:30:32 +0700

    notes on maps vs structs

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 }
 
 func printMap(c map[string]string){
-	for color,hex := range c{  // key,value range args param
+	for color,hex := range c{  // key,value range args param .  vars created must be used below
 		fmt.Println("color " , color, " key is " ,hex)
 	}
 }
@@ -26,3 +26,6 @@ func printMap(c map[string]string){
 // go is a pass by value language
 
 
+//maps vs structs :
+
+// https://www.udemy.com/course/go-the-complete-developers-guide/learn/lecture/7797370#notes  , 0:23
