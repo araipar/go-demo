64a727a9a1f600fd403c49ce52ecf1882ad6792d
Rai Paramaartha <araipars@gmail.com>
2022-05-19 00:22:49 +0700

    iterating through maps

--- main.go
+++ main.go
@@ -5,17 +5,24 @@ import "fmt"
 
 func main() {
 
-	// colors := map[string]string{
-	// 	"red" : "#ff0000",
-	// 	"green" : "#1234556",
-	// }
+	colors := map[string]string{
+		"red" : "#ff0000",
+		"green" : "#1234556",
+		"white" : "#fffff",
+	}
 
-	colors := make(map[string]string)
-	colors["white"] = "#ffffff" // add
 
-	delete(colors,"white") // delete
 
-	fmt.Println(colors)
+	printMap(colors)
 
 }
+
+func printMap(c map[string]string){
+	for color,hex := range c{  // key,value range args param
+		fmt.Println("color " , color, " key is " ,hex)
+	}
+}
+
 // go is a pass by value language
+
+
