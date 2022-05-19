871c06968fdebef48a35392a83d15ea58c20c92c
Rai Paramaartha <araipars@gmail.com>
2022-05-19 23:42:14 +0700

    interface in practice

--- main.go
+++ main.go
@@ -1,10 +1,41 @@
 package main
+import "fmt"
+
+type bot interface{  // cara  bacanya : tipe baru (bot) untunk handle segala type yang punya method getGreeting() 
+	getGreeting() string
 
 
+}
+
+type englishBot struct{}
+
+type spanishBot struct{} 
 
 func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+
+}
+
+func printGreeting(b bot){
+	fmt.Println(b.getGreeting())
+}
 
 
+
+func (eb englishBot) getGreeting() string{  //method 
+	// custom logic for generating english greeting
+	return "Hi there"
+}
+
+
+func (sb spanishBot) getGreeting() string{
+	// custom logic for generating spanis greeting
+
+	return "Hola"
 }
 
 
