5cd9020b97de1f967c535c1f1a99ef40a269c270
Rai Paramaartha <araipars@gmail.com>
2022-05-20 00:48:03 +0700

    io.copy

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 import ("fmt"
 		"net/http"
 		"os"
+		"io"
 	)
 func main() {
 
@@ -12,10 +13,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([]byte,99999)
-	resp.Body.Read(bs)
+	// bs := make([]byte,99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
 
-	fmt.Println(string(bs))
+	//3 line up there can be replaced by 1 line down here
+
+	io.Copy(os.Stdout, resp.Body)
 
 }
 
