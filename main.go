0b73a8b783e5c85558004dfea8951f2222d29023
Rai Paramaartha <araipars@gmail.com>
2022-05-20 00:07:17 +0700

    http request

--- main.go
+++ main.go
@@ -1,41 +1,23 @@
 package main
-import "fmt"
-
-type bot interface{  // cara  bacanya : tipe baru (bot) untunk handle segala type yang punya method getGreeting() 
-	getGreeting() string
-
-
-}
-
-type englishBot struct{}
-
-type spanishBot struct{} 
-
+import ("fmt"
+		"net/http"
+		"os"
+	)
 func main() {
-	eb := englishBot{}
-	sb := spanishBot{}
 
-	printGreeting(eb)
-	printGreeting(sb)
+	resp, err := http.Get("http://google.com")
+
+	if err != nil {
+		fmt.Println("Error:",err)
+		os.Exit(1)
+	}
+
+	fmt.Println(resp)
 
 }
 
-func printGreeting(b bot){
-	fmt.Println(b.getGreeting())
-}
 
 
 
-func (eb englishBot) getGreeting() string{  //method 
-	// custom logic for generating english greeting
-	return "Hi there"
-}
-
-
-func (sb spanishBot) getGreeting() string{
-	// custom logic for generating spanis greeting
-
-	return "Hola"
-}
 
 
